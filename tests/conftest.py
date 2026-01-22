ad130b5a233f580198b04a13a91ea7e4ba97d484
galina <lukinskaya.alina@gmail.com>
2026-01-22 17:29:43 -0500

    login test fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 
 import pytest
+from playwright.sync_api import Page
+
+from pages.home_page import HomePage
 from pages.login_page import LoginPage
 
 
@@ -18,8 +21,19 @@ def user_credentials():
 
 
 @pytest.fixture(scope="function")
+def page(page: Page, base_url) -> Page:
+    page.goto(base_url)
+    return page
+
+
+@pytest.fixture(scope="function")
 def login_page(page, base_url):
     return LoginPage(page, base_url=base_url)
+
+
+@pytest.fixture(scope="function")
+def home_page(page, base_url):
+    return HomePage(page, base_url=base_url)
 
 
 @pytest.fixture(scope="function")
